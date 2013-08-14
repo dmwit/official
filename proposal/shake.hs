f081de034c3db887e9ceb53faf0b9279e7c7a4d8
Daniel Wagner <daniel@wagner-home.com>
2013-08-14 10:59:51 -0400

    fix clean builds and presentation build

--- proposal/shake.hs
+++ proposal/shake.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Development.Shake
 import Development.Shake.FilePath
 import System.Posix.Env
@@ -6,16 +7,27 @@ import System.Posix.Env
 -- TODO: handle graphics files and stuff
 -- TODO: make a "clean" target
 
+system_ prog args = do
+	Exit _ <- command [] prog args
+	return ()
+
 sources = ["paper", "finished_work", "front_matter", "introduction", "related_work", "spreadsheets"]
 bibs = map (++".bib") ["bcp", "delta", "harmony", "spreadsheets", "symmetric"]
 sourcesWith s = [f ++ "." ++ s | f <- sources]
-pdflatex out = system' "pdflatex" ["-interaction=nonstopmode", dropExtension out]
+pdflatex  out = system' "pdflatex" ["-interaction=nonstopmode", dropExtension out]
+pdflatex_ out = system_ "pdflatex" ["-interaction=nonstopmode", dropExtension out]
+lowerExtension s = case takeExtension s of
+	"pdf" -> replaceExtension s "aux"
+	"aux" -> replaceExtension s "tex"
+	_ -> s
 
 main = shakeArgs shakeOptions $ do
 	want ["paper.pdf", "presentation.pdf"]
 	sourcesWith "aux" *>> \_ -> do
 		need (sourcesWith "tex")
-		pdflatex "paper"
+		-- when we haven't run bibtex yet, pdflatex will fail, so ignore that
+		-- failure here and report on it during the second run in the pdf rule
+		pdflatex_ "paper"
 	["*.blg", "*.bbl"] *>> \[blg, bbl] -> do
 		need (sourcesWith "aux" ++ bibs)
 		system' "bibtex" [dropExtension blg]
@@ -23,6 +35,10 @@ main = shakeArgs shakeOptions $ do
 		need (replaceExtension out "tex" : replaceExtension out "bbl" : sourcesWith "aux")
 		pdflatex out
 	"presentation.pdf" *> \out -> do
-		need [replaceExtension out "tex", replaceExtension out "aux"]
+		need ["presentation.aux"]
+		liftIO $ setEnv "TEXINPUTS" "..:" True
+		pdflatex out
+	"presentation.aux" *> \out -> do
+		need ["presentation.tex"]
 		liftIO $ setEnv "TEXINPUTS" "..:" True
 		pdflatex out
